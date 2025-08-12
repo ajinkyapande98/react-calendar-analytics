eaedd3ae6a92ed5359ff15b157a5021f747255aa
Ajinkya Pande <ajinkya.pande@tekditechnologies.com>
2025-08-12 17:03:10 +0530

    minor changes

--- src/store/calendarSlice.ts
+++ src/store/calendarSlice.ts
@@ -5,25 +5,33 @@ import { format } from 'date-fns';
 
 // Sample data
 const initialState: CalendarData = {
-    "01-09-2025": [
-        { "user_1": 1 },
-        { "user_2": 2 },
-        { "user_3": 3 },
-        { "user_4": 4 },
-    ],
-    "02-09-2025": [
-        { "user_1": 1 },
-        { "user_2": 2 },
-        { "user_3": 3 },
-        { "user_4": 4 },
-    ],
-    "03-09-2025": [
-        { "user_1": 1 },
-        { "user_2": 2 },
-        { "user_3": 3 },
-        { "user_4": 4 },
-    ],
-};
+  "01-09-2025": [
+    { "user_1": 1 },
+    { "user_2": 2 },
+    { "user_3": 3 },
+    { "user_4": 4 },
+  ],
+  "02-09-2025": [
+    { "user_1": 1 },
+    { "user_2": 2 },
+    { "user_3": 3 },
+    { "user_4": 4 },
+  ],
+  "03-09-2025": [
+    { "user_1": 4 },
+    { "user_2": 3 },
+    { "user_3": 2 },
+    { "user_4": 1 },
+  ],
+  "04-09-2025": [
+    { "user_1": 1 },
+    { "user_2": 2 },
+    { "user_3": 3 },
+    { "user_4": 4 },
+  ]
+}
+
+
 
 const calendarSlice = createSlice({
     name: 'calendar',
